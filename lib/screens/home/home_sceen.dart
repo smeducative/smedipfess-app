bcd30d1e020c2f15a88bec2ba4e52a0f9ea88261
akrindev <akrinmin@gmail.com>
2021-10-21 09:44:35 +0700

    fixed home screen

--- lib/screens/home/home_sceen.dart
+++ lib/screens/home/home_sceen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smedipfess/constants/theme.dart';
 import 'package:smedipfess/screens/components/custom_app_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -28,8 +29,54 @@ class _HomeScreenState extends State<HomeScreen> {
         shrinkWrap: true,
         physics: const BouncingScrollPhysics(),
         children: [
-          const Center(
-            child: Text('Home screen'),
+          Container(
+            height: 350,
+            color: Colors.white,
+            child: Stack(
+              children: [
+                Container(
+                  height: 240,
+                  width: double.infinity,
+                  color: AppColorTheme.kPrimary,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 10,
+                          left: 20,
+                        ),
+                        child: Text(
+                          'X-1 RPL',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: 'poppins',
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 8.0,
+                          left: 20,
+                        ),
+                        child: Text(
+                          'Syakirin Amin',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: 'inter',
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
